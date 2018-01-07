983a262c34f77348158905ca1210346a158a32be
KING SABRI <king.sabri@gmail.com>
2018-01-07 18:59:01 +0300

    refactor and import extentions

--- lib/jwtear.rb
+++ lib/jwtear.rb
@@ -6,9 +6,21 @@ require 'openssl'
 
 # JWTear
 require_relative 'jwtear/version'
-require_relative 'jwtear/utils'
 require_relative 'jwtear/error'
+require_relative 'jwtear/extensions'
+# require_relative 'jwtear/utils'
 require_relative 'jwtear/jwt'
+
+
+module  JWTear
+  String.class_eval do
+    include Extensions::Core::String
+  end
+  # NilClass.class_eval do
+  #   include Extensions::Core::NilClass
+  # end
+end
+
 
 # $LOAD_PATH << '../modules'
 # require_relative '../'
