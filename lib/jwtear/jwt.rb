a6d8907e12bd0bb7b9c8120b88333297691ecb14
KING SABRI <king.sabri@gmail.com>
2018-01-19 21:37:45 +0300

    refactor generate_token and bug fixes

--- lib/jwtear/jwt.rb
+++ lib/jwtear/jwt.rb
@@ -8,8 +8,8 @@ module JWTear
     include JWTear::Utils
 
     # @!attribute [rw] token [String] generated or parsed token
-    # @!attribute [rw] header [String] generated or parsed header
-    # @!attribute [rw] payload [String] generated or parsed payload
+    # @!attribute [rw] header [Hash]
+    # @!attribute [rw] payload [Hash]
     attr_accessor :token, :header, :payload
     # @!attribute [rw] alg [String] generated or parsed algorithm
     # @!attribute [rw] key [String] given encryption key
@@ -18,7 +18,7 @@ module JWTear
     # @!attribute [r] json [JSON] given or parsed json
     # @!attribute [r] hash [Hash] hash result of parsing given or generated json
     attr_reader :json, :hash
-    # @!attribute [r] signature [String] generated or parsed signature
+    # @!attribute [r] signature [String] generated or parsed signature.
     # @!attribute [r] rsa_private [String] generated private private key
     # @!attribute [r] rsa_public [String] generated or given public key
     attr_reader :signature, :rsa_private, :rsa_public
@@ -33,27 +33,28 @@ module JWTear
     #
     # @param token String
     def parse(token=@token)
+      is_token?(token)
       _token = token.split('.')
-      @header     = JSON.parse(Base64.urlsafe_decode64(_token[0]))
+      @header     = JSON.parse(decode(_token[0]))
       @type, @alg = @header['type'], @header['alg']
-      @payload    = JSON.parse(Base64.urlsafe_decode64(_token[1]))
-      @signature  = Base64.urlsafe_decode64(_token[2]) unless (_token[2].nil? or _token[2].empty?)
+      @payload    = JSON.parse(decode(_token[1]))
+      @signature  = decode(_token[2]) unless (_token[2].nil? or _token[2].empty?)
       set_hash_and_json
     end
 
     # build the hash and Json format from the parsed or generated token
     def set_hash_and_json
-      @json       = "#{@header.to_json}.#{@payload.to_json}.#{Base64.urlsafe_encode64(@signature, padding: false)}"
-      @hash       = {header: @header, payload: @payload, signature: Base64.urlsafe_encode64(@signature, padding: false)}
+      @hash       = {header: @header, payload: @payload, signature: encode(@signature)}
+      @json       = "#{@header.to_json}.#{@payload.to_json}.#{encode(@signature)}"
     end
 
     # generate signature
     #
     # @param data [String]. 'Base64.encode(header)'.'Base64.encode(payload)'>
-    # @param alg [String] supported algorithms: HS256 HS384, HS512, RS256, RS384, RS512, ES256, ES384, ES512
+    # @param alg [String] supported algorithms: @see [Algorithms#supported_algorithms]
     # @param key String
     #
-    # @return [String] the generate signature
+    # @return [Self] the generate signature
     #
     def generate_sig(data, alg, key)
       begin
@@ -66,9 +67,9 @@ module JWTear
             @rsa_private = rsa[:private_key]
             @signature   = rsa[:signature]
           when /^ES/
-            ecdsa(data, alg)
+            @signature = ecdsa(data, alg)
           when /none/i
-            none
+            @signature = none
           else
             raise AlgorithmUnknownError
         end
@@ -93,22 +94,24 @@ module JWTear
     end
 
     # generate JWT token
-    #   by default, generate_token uses the given json header to detect the algorithm. But it also accept to ignore than
-    #   and force it to you another algorithm.
+    #   by default, generate_token uses the given json header to detect the algorithm.
+    #   But it also accept to ignore that and force it to you another algorithm.
     #
     # @return [String] the generated token
     #
     def generate_token
-      @header    =  JSON.parse(@header)  unless @header.is_a?(Hash)
-      @payload   =  JSON.parse(@payload) unless @payload.is_a?(Hash)
-      @alg       = @header['alg'] if @alg.nil?
-      header    = Base64.urlsafe_encode64(@header.to_json, padding: false)
-      payload   = Base64.urlsafe_encode64(@payload.to_json, padding: false)
-      data      = "#{header}.#{payload}"
-      @signature = generate_sig(data, @alg, @key).signature
-      signature = encode(@signature)
-      token = [header, payload, signature].join('.')
-      parse(token)
+
+      @header   = JSON.parse(@header)  unless @header.is_a?(Hash)
+      @payload  = JSON.parse(@payload) unless @payload.is_a?(Hash)
+      @alg      = @header['alg'] if @alg.nil?   # if algorithm not forced, take if from the header
+
+      header_encoded    = encode(@header.to_json)
+      payload_encoded   = encode(@payload.to_json)
+      data              = "#{header_encoded}.#{payload_encoded}"
+      signature_encoded = encode(generate_sig(data, @alg, @key).signature)
+      token     = [header_encoded, payload_encoded, signature_encoded].join('.')
+
+      set_hash_and_json
 
       token
     end
